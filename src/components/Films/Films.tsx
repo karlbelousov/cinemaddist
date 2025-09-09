360d9c31272f34456fb8ee120e538212c641f06b
Karl Belousov <belousovkarl@gmail.com>
2025-09-09 15:45:03 +0600

    рефакторинг кода копонентов Main и Films : перенес компонент сортировки  в Films, отображение заглушки при пустом списке фильмов с сервера

--- src/components/Films/Films.tsx
+++ src/components/Films/Films.tsx
@@ -1,27 +1,30 @@
 import { Film } from "../../types/film";
 import FilmsEmpty from "../FilmsEmpty/FilmsEmpty";
 import FilmsList from "../FilmsList/FilmsList";
+import Sort from "../Sort/Sort";
 
 interface FilmsProps {
-  films: Film[];
+  allFilms: Film[];
 }
 
-function Films({ films }: FilmsProps) {
+function Films({ allFilms }: FilmsProps) {
   return (
-    <section className="films">
-      {films.length === 0 && <FilmsEmpty />}
-      {Films.length > 0 && (
-        <>
-          <FilmsList films={films} />
-          <FilmsList films={films.slice(0, 2)} title="Top rated" mode="extra" />
-          <FilmsList
-            films={films.slice(0, 2)}
-            title="Most commented"
-            mode="extra"
-          />
-        </>
-      )}
-    </section>
+    <>
+      {allFilms.length > 0 && <Sort />}
+      <section className="films">
+        {allFilms.length > 0 ? <FilmsList films={allFilms} /> : <FilmsEmpty />}
+        <FilmsList
+          films={allFilms.slice(0, 2)}
+          title="Top rated"
+          mode="extra"
+        />
+        <FilmsList
+          films={allFilms.slice(0, 2)}
+          title="Most commented"
+          mode="extra"
+        />
+      </section>
+    </>
   );
 }
 
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,18 +1,20 @@
 import { useGetFilmsQuery } from "../../services/appApi";
 import Films from "../Films/Films";
+import FilmsEmpty from "../FilmsEmpty/FilmsEmpty";
 import Loader from "../Loader/Loader";
 import MainNavigation from "../MainNavigation/MainNavigation";
-import Sort from "../Sort/Sort";
 
 function Main() {
-  const {data: films, isLoading } = useGetFilmsQuery();
+  const { data: allFilms, isLoading } = useGetFilmsQuery();
 
   return (
     <main className="main">
       <MainNavigation />
       {isLoading && <Loader />}
-      {!isLoading && <Sort />}
-      {!isLoading && films && <Films films={films} />}
+      {!isLoading && allFilms && allFilms.length > 0 && (
+        <Films allFilms={allFilms} />
+      )}
+      {!isLoading && allFilms && allFilms.length === 0 && <FilmsEmpty />}
     </main>
   );
 }
